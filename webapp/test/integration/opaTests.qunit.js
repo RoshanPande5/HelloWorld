8d8ffcb66ab43d6ea83ccf825b24b594dea3a0d1
RoshanPande5 <roshanpande2233@gmail.com>
2024-12-17 08:38:43 +0000

    Commit Hello World

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["mm/helloworld/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"mm/hello_world/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
